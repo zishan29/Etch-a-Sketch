a464342ef4c8c0d81f0e4b135faf7e8ef57b1969
Zishan <szishan2902@gmail.com>
2023-06-12 21:04:50 +0530

    Add functionality to change color on hover

--- script.js
+++ script.js
@@ -2,18 +2,42 @@ function createGrid(input) {
   const container = document.querySelector('.container');
   container.replaceChildren();
     for (let i = 1; i <= input; i++) {
-      const row = document.createElement('div');
-      row.className = 'row';
+      const column = document.createElement('div');
+      column.className = 'column';
       for (let j = 1; j <= input; j++) {
         const pixel = document.createElement('div');
         pixel.className = 'pixel';
-        row.appendChild(pixel);
+        column.appendChild(pixel);
       }
-      container.appendChild(row);
+      container.appendChild(column);
     }
 }
+function colorPixel() {
+  const pixels = document.querySelectorAll(".pixel");
+  pixels.forEach((pixel) => {
+    pixel.addEventListener('mouseover', e => e.target.classList.add('color'));
+  })
+}
 
+createGrid(16);
+colorPixel();
 const input = document.querySelector(".input");
+input.value = 16;
 input.addEventListener("input", (event) => {
   createGrid(event.target.value);
+  colorPixel();
+});
+
+const reset = document.querySelector("#reset");
+reset.addEventListener('click', function() {
+  createGrid(16);
+  colorPixel();
+  input.value = 16;
 })
+
+const erase = document.querySelector("#erase");
+erase.addEventListener('click', function() {
+  createGrid(input.value);
+  colorPixel();
+})
+
